c504d1b87a169295623f903e77254240fcb4df40
Kaushi2002Nishu <Kaushin760@gmail.com>
2026-05-02 16:54:20 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 
 app.use(express.json());
 
+dotenv.config();
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+const PORT = process.env.PORT || 5000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose
+.connect(MONGOURL)
+.then(() => {
+    console.log("MongoDB connected succesfully");
+
+    app.listen(PORT,() => {
+      console.log(`Server is running on Port : ${PORT}`);
+    });
+})
+.catch((error) => console.log(error));
+app.use("/api/workers",workerRoutes);
