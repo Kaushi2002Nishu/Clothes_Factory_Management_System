fd1452c7183604a266833769501e007963f9ab24
Kaushi2002Nishu <Kaushin760@gmail.com>
2026-05-11 14:06:54 +0530

    Update WorkerController.js

--- controllers/WorkerController.js
+++ controllers/WorkerController.js
@@ -9,3 +9,18 @@ exports.getWorkers = async(req,res) => {
     const worker = await Worker.find();
     res.json(workers);
 };
+
+exports.updateWorkers = async(req, res) => {
+    const workers = await Worker.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new : true }
+    );
+    res.json(workers);
+};
+
+exports.deleteWorker = async (req , res) => {
+    await Worker.findByIdAndDelete(req.params.id);
+    res.json({ message : "Worker deleted" });
+};
+
