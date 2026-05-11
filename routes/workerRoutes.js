175bf70b16b568678e92be351eb35b31336e5d5b
Kaushi2002Nishu <Kaushin760@gmail.com>
2026-05-11 15:29:48 +0530

    Update WorkerRoute.js

--- routes/workerRoutes.js
+++ routes/workerRoutes.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const { createWorker, getWorkers, deleteWorker} = require("../controllers/WorkerController");
+
+router.post("/", createWorker);
+router.get("/", getWorkers);
+router.delete("/:id", deleteWorker);
+
+module.exports = router;
